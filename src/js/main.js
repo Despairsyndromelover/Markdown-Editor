11b541372e9ce74d92fbc63928ccfc70d02c70bc
DESKTOP-512G8GI\undefined <podpivomatveyka@gmail.com>
2024-09-12 19:29:23 +0500

    12.09 layout + some functional done

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
 import '../scss/style.scss';
+
+const rawInput = document.querySelector('#raw-input');
+const preview = document.querySelector('#preview');
+
+console.log(rawInput);
+console.log(preview);
+
+function updatePreview() {
+  const formattedInputValue = rawInput.value.replaceAll('\n', `<br/>`);
+  preview.innerHTML = formattedInputValue;
+}
+
+rawInput.addEventListener('input', updatePreview);
