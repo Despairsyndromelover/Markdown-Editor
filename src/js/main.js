8b16e534f8849487611a6013cc10dc6f5794f07c
DESKTOP-512G8GI\undefined <podpivomatveyka@gmail.com>
2024-09-16 20:24:56 +0500

    list element done + structure reformat

--- src/js/main.js
+++ src/js/main.js
@@ -11,52 +11,161 @@ const getFormattedInputLines = () => {
 };
 
 const deletePreviewInner = () => (preview.innerHTML = '');
+
+const headings = [
+  { tag: 'h1', markdownSyntax: '#', class: 'heading1' },
+  { tag: 'h2', markdownSyntax: '##', class: 'heading2' },
+  { tag: 'h3', markdownSyntax: '###', class: 'heading3' },
+  { tag: 'h4', markdownSyntax: '####', class: 'heading4' },
+  { tag: 'h5', markdownSyntax: '#####', class: 'heading5' },
+  { tag: 'h6', markdownSyntax: '######', class: 'heading6' },
+];
+
+const getHeadingTemplate = (heading, line, index) => {
+  return `<${heading.tag} 
+  class="${heading.class}" 
+  id=${index}>
+  ${line.trimStart().slice(heading.markdownSyntax.length, line.length)}
+  </${heading.tag}>`;
+};
 const createHeadings = (lines) => {
-  lines.forEach((line, index) => {
-    if (line.trimStart().slice(0, 6) === '######') {
+  lines.forEach((currentLine, index) => {
+    if (currentLine.startsWith(headings[5].markdownSyntax)) {
       preview.insertAdjacentHTML(
         'beforeend',
-        `<h6 class="heading6" id=${index}>${line.slice(6, line.length)}</h6>`
+        getHeadingTemplate(headings[5], currentLine, index)
       );
-    } else if (line.trimStart().slice(0, 5) === '#####') {
+    } else if (currentLine.startsWith(headings[4].markdownSyntax)) {
       preview.insertAdjacentHTML(
         'beforeend',
-        `<h5 class="heading5" id=${index}>${line.slice(5, line.length)}</h5>`
+        getHeadingTemplate(headings[4], currentLine, index)
       );
-    } else if (line.trimStart().slice(0, 4) === '####') {
+    } else if (currentLine.startsWith(headings[3].markdownSyntax)) {
       preview.insertAdjacentHTML(
         'beforeend',
-        `<h4 class="heading4" id=${index}>${line.slice(4, line.length)}</h4>`
+        getHeadingTemplate(headings[3], currentLine, index)
       );
-    } else if (line.trimStart().slice(0, 3) === '###') {
+    } else if (currentLine.startsWith(headings[2].markdownSyntax)) {
       preview.insertAdjacentHTML(
         'beforeend',
-        `<h3 class="heading3" id=${index}>${line.slice(3, line.length)}</h3>`
+        getHeadingTemplate(headings[2], currentLine, index)
       );
-    } else if (line.trimStart().slice(0, 2) === '##') {
+    } else if (currentLine.startsWith(headings[1].markdownSyntax)) {
       preview.insertAdjacentHTML(
         'beforeend',
-        `<h2 class="heading2" id=${index}>${line.slice(2, line.length)}</h2>`
+        getHeadingTemplate(headings[1], currentLine, index)
       );
-    } else if (line.trimStart().slice(0, 1) === '#') {
+    } else if (currentLine.trimStart().startsWith(headings[0].markdownSyntax)) {
       preview.insertAdjacentHTML(
         'beforeend',
-        `<div class="heading1" id=${index}>${line
-          .trim()
-          .slice(1, line.length)}</div>`
-      );
-    } else {
-      preview.insertAdjacentHTML(
-        'beforeend',
-        `<div class="element" id=${index}>${line}</div>`
+        getHeadingTemplate(headings[0], currentLine, index)
       );
     }
   });
 };
+
+const lists = [{ tag: `li`, markdownSyntax: '-', class: 'listElement' }];
+const getListItemTemplate = (list, line, index) => {
+  return `<${list.tag} 
+  class="${list.class}" 
+  id=${index}>
+  ${line.slice(list.markdownSyntax.length, line.length)}
+  </${list.tag}>`;
+};
+
+const checkHeadings = () => {
+  const lines = getFormattedInputLines();
+  lines.forEach((currentLine, index) => {
+    if (currentLine.startsWith(headings[5].markdownSyntax)) {
+      preview.insertAdjacentHTML(
+        'beforeend',
+        getHeadingTemplate(headings[5], currentLine, index)
+      );
+    } else if (currentLine.startsWith(headings[4].markdownSyntax)) {
+      preview.insertAdjacentHTML(
+        'beforeend',
+        getHeadingTemplate(headings[4], currentLine, index)
+      );
+    } else if (currentLine.startsWith(headings[3].markdownSyntax)) {
+      preview.insertAdjacentHTML(
+        'beforeend',
+        getHeadingTemplate(headings[3], currentLine, index)
+      );
+    } else if (currentLine.startsWith(headings[2].markdownSyntax)) {
+      preview.insertAdjacentHTML(
+        'beforeend',
+        getHeadingTemplate(headings[2], currentLine, index)
+      );
+    } else if (currentLine.startsWith(headings[1].markdownSyntax)) {
+      preview.insertAdjacentHTML(
+        'beforeend',
+        getHeadingTemplate(headings[1], currentLine, index)
+      );
+    } else if (currentLine.trimStart().startsWith(headings[0].markdownSyntax)) {
+      preview.insertAdjacentHTML(
+        'beforeend',
+        getHeadingTemplate(headings[0], currentLine, index)
+      );
+    }
+  });
+};
+const checkLists = () => {
+  const lines = getFormattedInputLines();
+  lines.forEach((currentLine, index) => {
+    if (currentLine.trimStart().startsWith('-')) {
+      preview.insertAdjacentHTML(
+        'beforeend',
+        getListItemTemplate(lists[0], currentLine, index)
+      );
+    }
+  });
+};
+
 const updatePreview = () => {
   const lines = getFormattedInputLines();
   deletePreviewInner();
-  createHeadings(lines);
+  lines.forEach((currentLine, index) => {
+    if (currentLine.trimStart().startsWith(headings[5].markdownSyntax)) {
+      preview.insertAdjacentHTML(
+        'beforeend',
+        getHeadingTemplate(headings[5], currentLine, index)
+      );
+    } else if (currentLine.trimStart().startsWith(headings[4].markdownSyntax)) {
+      preview.insertAdjacentHTML(
+        'beforeend',
+        getHeadingTemplate(headings[4], currentLine, index)
+      );
+    } else if (currentLine.trimStart().startsWith(headings[3].markdownSyntax)) {
+      preview.insertAdjacentHTML(
+        'beforeend',
+        getHeadingTemplate(headings[3], currentLine, index)
+      );
+    } else if (currentLine.trimStart().startsWith(headings[2].markdownSyntax)) {
+      preview.insertAdjacentHTML(
+        'beforeend',
+        getHeadingTemplate(headings[2], currentLine, index)
+      );
+    } else if (currentLine.trimStart().startsWith(headings[1].markdownSyntax)) {
+      preview.insertAdjacentHTML(
+        'beforeend',
+        getHeadingTemplate(headings[1], currentLine, index)
+      );
+    } else if (currentLine.trimStart().startsWith(headings[0].markdownSyntax)) {
+      preview.insertAdjacentHTML(
+        'beforeend',
+        getHeadingTemplate(headings[0], currentLine, index)
+      );
+    } else if (currentLine.trimStart().startsWith('-')) {
+      preview.insertAdjacentHTML(
+        'beforeend',
+        getListItemTemplate(lists[0], currentLine, index)
+      );
+    } else {
+      preview.insertAdjacentHTML(
+        'beforeend',
+        `<div class="element" id=${index}>${currentLine}</div>`
+      );
+    }
+  });
 };
-
 markdownInput.addEventListener('input', updatePreview);
